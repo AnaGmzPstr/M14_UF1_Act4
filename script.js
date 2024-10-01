0995079a2af67c0fb0c4cbe646d9ad9688e4e4af
Ana Gómez Pastor <165878099+AnaGmzPstr@users.noreply.github.com>
2024-10-01 16:29:38 +0000

    inici exercicis

---
+++ script.js
@@ -0,0 +1,17 @@
+//Part 1
+
+//Exercici 1
+function exercici1_1(){
+    let resultat = document.getElementById("res1");
+    const estudiant = {
+        nom: 'Joan',
+        edat: 20,
+        notes: {
+            matematiques: 8,
+            programacio: 9,
+            fisica: 7
+        }
+    };
+    
+    
+}
