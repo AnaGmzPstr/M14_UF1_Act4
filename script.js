0dc9c1baa0ecd9f8c69e56eee4f4832d8c13e6b6
Ana Gómez Pastor <165878099+AnaGmzPstr@users.noreply.github.com>
2024-10-01 16:40:05 +0000

    Primera part feta

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ function exercici1_1(){
             fisica: 7
         }
     };
-    
-    
+    resultat.innerHTML = `${estudiant.nom} té ${estudiant.edat} anys i ha tret un ${estudiant.notes.matematiques} en matemàtiques`;
+
 }
+
+//Exercici 2
+function exercici1_2(){
+    let resultat = document.getElementById("res2");
+    const coordenades = [12, 5, 8];
+    resultat.innerHTML =  `Les coodenades són x=${coordenades[0]}, y=${coordenades[1]}  i z=${coordenades[2]}`;
+}
+
+//Part 2
